0c6a7162802cc828bdba4ba3077011092a34344e
kennck <kennettecanales@gmail.com>
2021-05-11 08:24:11 +0800

    udpate

--- src/router/dev_routes/payhiram.js
+++ src/router/dev_routes/payhiram.js
@@ -210,16 +210,14 @@ export default{
     meta: {
       tokenRequired: true
     }
-  },
-  // {
-  //   path: '/thread/:code?',
-  //   name: 'thread',
-  //   component: resolve => require(['modules/messengervue/Payhiram.vue'], resolve),
-  //   meta: {
-  //     tokenRequired: true
-  //   }
-  // },
-  {
+  }, {
+    path: '/thread/:code?',
+    name: 'thread',
+    component: resolve => require(['modules/messengervue/Payhiram.vue'], resolve),
+    meta: {
+      tokenRequired: true
+    }
+  }, {
     path: '/paymentConfirmation/:email/:userCode/:depositCode',
     name: 'paymentConfirmation',
     component: resolve => require(['modules/deposit/PaymentConfirmation.vue'], resolve),
